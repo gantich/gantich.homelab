38375f04bb0d1d20a566cda868a4e20efb9729a3
Guillermo Antich <gantich@users.noreply.github.com>
2023-11-16 10:12:56 +0100

    Update update script

--- update-docker-containers.sh
+++ update-docker-containers.sh
@@ -23,12 +23,18 @@ BOLD_CYAN="${BOLD}${CYAN}"
 BOLD_WHITE="${BOLD}${WHITE}"
 BOLD_GRAY="${BOLD}${GRAY}"
 
-echo -e "${BOLD}${GREEN}  ____            _        _                   _   _           _       _            "
-echo -e "${BOLD}${GREEN} / ___|___  _ __ | |_ __ _(_)_ __   ___ _ __  | | | |_ __   __| | __ _| |_ ___ _ __ "
-echo -e "${BOLD}${GREEN}| |   / _ \| '_ \| __/ _` | | '_ \ / _ \ '__| | | | | '_ \ / _` |/ _` | __/ _ \ '__|"
-echo -e "${BOLD}${GREEN}| |__| (_) | | | | || (_| | | | | |  __/ |    | |_| | |_) | (_| | (_| | ||  __/ |   "
-echo -e "${BOLD}${GREEN} \____\___/|_| |_|\__\__,_|_|_| |_|\___|_|     \___/| .__/ \__,_|\__,_|\__\___|_|   "
-echo -e "${BOLD}${GREEN}                                                    |_|                             "
+header() {
+cat<<"EOT"
+  ____            _        _                   _   _           _       _            
+ / ___|___  _ __ | |_ __ _(_)_ __   ___ _ __  | | | |_ __   __| | __ _| |_ ___ _ __ 
+| |   / _ \| '_ \| __/ _` | | '_ \ / _ \ '__| | | | | '_ \ / _` |/ _` | __/ _ \ '__|
+| |__| (_) | | | | || (_| | | | | |  __/ |    | |_| | |_) | (_| | (_| | ||  __/ |   
+ \____\___/|_| |_|\__\__,_|_|_| |_|\___|_|     \___/| .__/ \__,_|\__,_|\__\___|_|   
+                                                    |_|                             
+EOT
+}
+
+header
 
 echo "${BOLD_MAGENTA}Download the latest docker-compose.yml from GitHub${RESET}"
 curl -O https://raw.githubusercontent.com/gantich/gantich.homelab/master/docker-compose.yaml
