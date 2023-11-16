24928c89520dae2d80c3b99645b049a41160f111
Guillermo Antich <gantich@users.noreply.github.com>
2023-11-16 09:33:51 +0100

    Update update containers script

--- update-docker-containers.sh
+++ update-docker-containers.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Download the latest docker-compose.yml from GitHub
+echo "Download the latest docker-compose.yml from GitHub"
 curl -O https://raw.githubusercontent.com/gantich/gantich.homelab/master/docker-compose.yaml
 
-# Stop and remove all running containers
+echo "Stop all running containers"
 docker stop $(docker ps -aq)
+
+echo "Remove all containers"
 docker rm $(docker ps -aq)
 
-# Pull the latest images for all containers
+echo "Pull the latest images for all containers"
 docker compose pull
 
-# Recreate and start all containers
+echo "Recreate and start all containers"
 docker compose up -d
